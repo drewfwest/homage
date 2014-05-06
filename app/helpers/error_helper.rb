f67fb59701c78100f8e77842126ef146cb753782
Drew West <drewfwest@gmail.com>
2014-05-05 22:20:55 -0700

    User data persists in sign in forms if attempted sign in fails   *Data passed through flash

--- app/helpers/error_helper.rb
+++ app/helpers/error_helper.rb
@@ -1,13 +1,21 @@
-def error_messages(object_with_errors)
-  if object_has_errors?(object_with_errors)
-    all_error_messages = object_with_errors.errors.full_messages.map do |message|
-      error_message = message.split(" ")
-      error_message.delete_at(0)
-      error_message.join(" ")
+helpers do
+  def error_messages(object_with_errors)
+    if object_has_errors?(object_with_errors)
+      all_error_messages = object_with_errors.errors.full_messages.map do |message|
+        error_message = message.split(" ")
+        error_message.delete_at(0)
+        error_message.join(" ")
+      end
     end
   end
-end
 
-def object_has_errors?(object)
-  object.errors.full_messages.length > 0
+  def object_has_errors?(object)
+    object.errors.full_messages.length > 0
+  end
+
+  def get_messages(params)
+    input = []
+    params.each_value { |user_input| input << user_input }
+    return input
+  end
 end
