a3e448dbe4381b9aa553d6997f9ecb593c34a479
Drew West <drewfwest@gmail.com>
2014-05-06 17:02:34 -0700

    'Foundation' code saved for popup windows

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
-
+  $(this).foundation();
   // Optional - Use AJAX to send an HTTP DELETE request for the sign-out link
-  
-
 });
+
+
+$('a.reveal-link').trigger('click');
+$('a.close-reveal-modal').trigger('click');
